0b4ded1a53f7a028f91748dd846f819dbec17b6d
Robertabreu23 <abreudelgado2@gmail.com>
2025-03-05 12:48:53 -0400

    Add files via upload

---
+++ Ejemplo 1 de herencia (Vehiculos)/Ejemplo1 Vehiculos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Vehiculo
+{
+    public required string Marca { get; set; }
+    public int Año { get; set; }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Marca: {Marca}, Año: {Año}");
+    }
+}
+
+class Auto : Vehiculo
+{
+    public int Puertas { get; set; }
+
+    public void MostrarDetalles()
+    {
+        MostrarInfo();
+        Console.WriteLine($"Puertas: {Puertas}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Auto miAuto = new Auto { Marca = "Toyota", Año = 2022, Puertas = 4 };
+        miAuto.MostrarDetalles();
+    }
+}
+
+
